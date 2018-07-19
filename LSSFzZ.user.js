4e396a0c94c26de430c4a2a582485e6c75cc3eac
Tobias <accessburn@tobias-riefer.de>
2018-07-19 11:01:24 +0200

    Update LSSFzZ.user.js

--- LSSFzZ.user.js
+++ LSSFzZ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LSS Fahrzeugzähler
-// @version      0.1
+// @version      0.2
 // @description  Zählt die noch benötigten Fahrzeuge bei einer GSL o.ä.
 // @author       accessburn
 // @include      *://www.leitstellenspiel.de/missions/*
