76863bd82fc0650acc71388a75c6237ff9b371b2
Tobias <accessburn@tobias-riefer.de>
2018-07-19 10:59:40 +0200

    Create LSSFzZ..user.js

---
+++ LSSFzZ..user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         LSS Fahrzeugzähler
+// @version      0.1
+// @description  Zählt die noch benötigten Fahrzeuge bei einer GSL o.ä.
+// @author       accessburn
+// @include      *://www.leitstellenspiel.de/missions/*
+// @updateURL    https://github.com/LennardTFD/lss-workers/raw/master/neededWorkers.user.js
+// @downloadURL  https://github.com/LennardTFD/lss-workers/raw/master/neededWorkers.user.js
+// @grant        none
+// @run          document-start
+// ==/UserScript==
+
+var node = document.getElementsByClassName('alert alert-danger')[0]; // rotes Div auslesen
+var nurcontent = node.textContent;                                   // Nur Text, kein HTML extrahieren
+nurcontent.replace('Zusätzlich benötigte Fahrzeuge: ', '');          // Anfangstext entfernen
+var array_Fahrzeug = nurcontent.slice(38).split(',');                // Array anlegen
+
+var NurDieZahl = '0';
+var AnzahlFahrzeuge = 0;
+array_Fahrzeug.forEach(function(einArrayElement) {
+    NurDieZahl = einArrayElement.split(' ');
+    AnzahlFahrzeuge = AnzahlFahrzeuge + parseInt(NurDieZahl[1]);
+});
+document.getElementsByClassName('alert alert-danger')[0].innerHTML = node.textContent + "<br /><br />Es fehlen " + AnzahlFahrzeuge + " Fahrzeuge!";
